efa401c23f202186eaa15ddbc28f5246d53efc7e
SHINBO1972 <jhlee@shinbonet.com>
2025-07-21 09:19:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,10 @@ document.addEventListener('DOMContentLoaded', function() {
         sectionObserver.observe(section);
     });
 });
+
+// HTML 문서가 모두 로드된 후 코드가 실행되도록 합니다.
+document.addEventListener('DOMContentLoaded', function() {
+
+    /* ▼▼▼ 이 부분이 새로 추가된 햄버거 메뉴 기능입니다 ▼▼▼ */
+    const hamburgerButton = document.getElementById('hamburger-button');
+    const mainNav = document.getElementById('main
